ac51fbca733bfe05bd4d452162886a232cfd2e96
Gabriel Alves <gabrielemailpessoal02@gmail.com>
2023-04-19 00:33:16 -0300

    atualizar nome do repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Desafio-01-ignite',
+  base: '/Desafio-Todo-01',
   plugins: [react()],
 })
